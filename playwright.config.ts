db5910851d490d053bf226e1ab32b392ba47f607
bagyinszkid <daniel.bagyinszki@nixs.com>
2025-02-18 12:54:44 +0100

    Removed unused parts of code and infrastructure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
-
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-/*
-  I've used "${process.env.ENV}'" before changing it to "+ process.env.ENV" because it didn't work 
-  with either the offical way (using __dirname) or with the $ relative path;
-  Found more information on dotenv and relative path: 
-  https://dev.to/eriesgo/dotenv-and-relative-paths-fp2
-
-*/
-// currently dotenv config is unused, part of earlier attempt
-
-dotenv.config({ 
- 
-  path: './env/.env.' + process.env.ENV,
-
-});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -51,12 +30,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-  /*
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-  */
     {
       name: 'qa-firefox',
       use: 
@@ -93,38 +66,5 @@ export default defineConfig({
         browserName: 'webkit',
       }
     },
-    // project should use Chrome and Safari, branded or engine was not specified
-    /*
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    */
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
